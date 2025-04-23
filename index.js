1499bec6a48734551221b0916e1d2a45c0b4f741
jairsantos123 <jairabvl@hotmail.com>
2025-04-23 16:58:00 -0300

    js ok

---
+++ index.js
@@ -0,0 +1,8 @@
+// Usando operadores aritméticos para calcular a média de três números
+let peso = Number 147;
+let altura = Number 1.84;
+
+let soma = peso + altura;
+let media = peso / (altura * altura )2;
+
+console.log(`A média de ${num1}, ${num2} e ${num3} é: ${media}`);
